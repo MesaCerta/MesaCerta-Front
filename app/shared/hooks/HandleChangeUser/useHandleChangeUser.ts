31c8471cb8eaa2f3cb0e52727db1883535ca4b9f
Henrique Almeida <henrique.almeida.dev@gmail.com.>
2025-04-24 12:58:47 -0300

    fix: correção do inital do handleChangeUser.

--- app/shared/hooks/HandleChangeUser/useHandleChangeUser.ts
+++ app/shared/hooks/HandleChangeUser/useHandleChangeUser.ts
@@ -14,6 +14,7 @@ const useHandleChangeUser = (initialData: Partial<IUserData> = {}) => {
     phone: "",
     birthdate: "",
     cpf: "",
+    restaurants: initialData.restaurants ?? [],
     ...initialData,
   });
 
